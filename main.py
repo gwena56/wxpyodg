f7ed30cbca0f11b80c4264e8ee7e378f906c8c9e
gwena56 <gwena56@gmail.com>
2016-02-15 11:05:47 +0100

    bugs correction

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from odglib import *
 import argparse
 import wx
 odg = odgSrc()
-odg.setUI("alarme.odg") #set User Interface from odg file
+odg.setUI("carpc.odg") #set User Interface from odg file
 #odg.setObjects("carpc.ini") # set User interface Command from ini file
 app = wx.App() # on garde un peu de controle sur l'app
 odg.initUI() # on lance interface utilisateur
