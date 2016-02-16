081af07666af4b3899ad3553716602700a3322ee
gwena56 <gwena56@gmail.com>
2016-02-16 14:26:15 +0100

    update

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from odglib import *
 import argparse
 import wx
 odg = odgSrc()
-odg.setUI("carpc.odg") #set User Interface from odg file
-#odg.setObjects("carpc.ini") # set User interface Command from ini file
+odg.setUI("pager.odg") #set User Interface from odg file
+odg.setParams("pager.ini") # set User interface UI,GENERAL params such HOVER_MASK color
 app = wx.App() # on garde un peu de controle sur l'app
 odg.initUI() # on lance interface utilisateur
 odg.window.Centre() #on centre la fenetre principale
 odg.window.Show(True)#on affiche tout
 app.MainLoop()
-# et c'est tout !!!'
+# et c'est tout !!!
--- odglib.py
+++ odglib.py
@@ -14,21 +14,25 @@ import ConfigParser as ini
 from StringIO import StringIO
 import uiFunction
 # definition variables pour tous les scripts
+global ERR_ODG, ERR_SYNTAX, HOVER_MASK, POINT, PLATFORM, CHECK_UPDATE,DO_BUILD,BUILD_NAME
 ERR_ODG = "Not a odg file."
 ERR_SYNTAX = "Syntax Error"
+# préférences standard de certaines variables de l'UI 
+PLATFORM = "LINUX" # A CAUSE DE LA COMPATIBILITE DES CHEMIN DE FICHIERS : file path ... LINUX,RASPBERRY_PI,MACOSX,WINDOWS
+HOVER_MASK = (255,255,255,255) # couleur des formes autocalculées pour l'hover des boutons
 # constant
 POINT = 0.03527777777778
 # GLOBAL
-global file, odg, ptr, data, root, uiPage, Example, window, background, config,iniObjects, uiObjects
+global file, odg, ptr, data, root, uiPage, Example, window, background, config,iniParams, uiObjects
 #def
 def S2P(a):
+    global POINT
     return int(round(float(a[0:-2])/POINT)) #-2 enleve la mesure cm
 def scale_bitmap(bitmap, width, height):
     image = wx.ImageFromBitmap(bitmap)
     image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
     result = wx.BitmapFromImage(image)
     return result
-
 # CLASS
 class odgSrc:
     """odgScr"""
@@ -50,24 +54,30 @@ class odgSrc:
                 self.root = ET.fromstring(data)
             return self.root
         
-    def setObjects(self,txt):
+    def setParams(self,txt):
+        global HOVER_MASK, PLATFORM
         """"setObjects"""
         config = ini.ConfigParser()
         config.read(txt)
-        Objects = config.sections()
+        sections = config.sections()
+        print sections
         ph =""
-        for object in Objects:
-            ph += "'"+object+"' :{"
-            options = config.options(object)
-            for option in options:
-                val = config.get(object,option)
-                ph += "'"+ option + "' : '"+val+"',"
+        for section in sections:
+            ph += "'"+section.upper()+"' :{"
+            params = config.options(section)
+            for param in params:
+                val = config.get(section,param)
+                ph += "'"+ param.upper() + "' : '"+val+"',"
             ph+= "} , "
-        
         ph = "{"+ph[0:-1]+"}"
-        #print ph
-        self.iniObjects = dict(ast.literal_eval(ph))
-        return self.iniObjects
+        self.iniParams = dict(ast.literal_eval(ph))
+        # definiton de paramètres importants
+        try :
+            HOVER_MASK = eval(self.iniParams['UI']['HOVER_MASK'])
+            PLATFORM = self.iniParams['GENERAL']['PLATFORM']
+        except KeyError:
+            """RIEN"""
+        return self.iniParams
     
     def drawBackgroundIO(self,ou,path):
         """drawBackground"""
@@ -105,6 +115,11 @@ class odgSrc:
                 if typeObject == "circle" or typeObject == "ellipse":
                     typeObject = "ellipse"
                     nom = self.ptr[i][0][0].text
+                    try :
+                        action = self.ptr[i][1][0].text
+                    except IndexError:
+                        action ="none"
+                    temp += ",'action':'"+action+"'"
                     self.uiObjects[nom] = dict(ast.literal_eval(temp + "}"))
                 if typeObject == "frame":
                     h = self.ptr[i][0].attrib
@@ -121,18 +136,21 @@ class odgSrc:
         return self.uiPage
     
     def CreateHoverPng(self,nom,x,y,width,height):
+        # on précise les global nécéssaires -> c'est surtout des variables du .ini
+        global HOVER_MASK
         #print nom,x,y,width,height
         img = Image.open("./ui/Pictures/background.png")
         #bb = (int(x),int(y),int(width),int(height))
-        crop_rectangle = (int(x),int(y),int(x)+int(width),int(y)+int(height))
+        crop_rectangle = (int(x),int(y),int(x)+int(width)+2,int(y)+int(height)+2)
         cropped_im = img.crop(crop_rectangle)#.convert('RGBA')
         cropped_im.save("./ui/Pictures/"+nom+".png")
         poly = Image.new('RGBA', cropped_im.size)
         pdraw = ImageDraw.Draw(poly)
         bb = (0,0,int(width),int(height))
-        pdraw.ellipse(bb, fill = (255,255,255,84))
+        pdraw.ellipse(bb, fill = HOVER_MASK) #(255,255,255,84)
         cropped_im.paste(poly,mask=poly)
         cropped_im.save("./ui/Pictures/"+nom+"_hover.png")
+        
 #### defS utilisant wxPython
     def uiMake(self):
         """Example STD_PUSHBT_ONOFF -> 
@@ -140,7 +158,6 @@ class odgSrc:
         PUSHBT_ = this is a push button
         ONOFF = it's name"""
         for uiObject in self.uiObjects.keys():
-            print uiObject
             obj = self.uiObjects[uiObject]
             nom = uiObject.split("_")
             if nom[1]=="PUSHBT":
@@ -148,13 +165,13 @@ class odgSrc:
                 
     def pushbutton(self,obj,nom):
         """bouton poussoir"""
-        print obj
         if nom[0]=="STD":
            self.CreateHoverPng(nom[2],obj['x'],obj['y'],obj['width'],obj['height'])
            nImg = wx.Image("./ui/Pictures/"+nom[2]+".png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
            hImg = wx.Image("./ui/Pictures/"+nom[2]+"_hover.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
            #(int(obj['x']),int(obj['y'])),(int(obj['width']),int(obj['height']))
-           vars()[nom[2]] = wx.BitmapButton(self.appBackground, -1, nImg,(int(obj['x']),int(obj['y'])), (int(obj['width']),int(obj['height'])), style=wx.BU_AUTODRAW|wx.NO_BORDER )
+           #, style=wx.BU_AUTODRAW|wx.NO_BORDER 
+           vars()[nom[2]] = wx.BitmapButton(self.appBackground, -1, nImg,(int(obj['x'])-1,int(obj['y'])-1), (int(obj['width']),int(obj['height'])), style=wx.BU_AUTODRAW|wx.NO_BORDER)
            vars()[nom[2]].SetBitmap(nImg)
            vars()[nom[2]].SetBitmapSelected(nImg)
            vars()[nom[2]].SetBitmapHover(hImg)
@@ -162,7 +179,6 @@ class odgSrc:
             """NOP"""
         else :
             """ Draw RED X ON IMAGE BLOC """
-        
 
     def drawBackground(self,ou,path):
         """drawBackground"""
@@ -181,4 +197,4 @@ class odgSrc:
         self.appBackground = wx.Panel(self.window, -1)
         self.drawBackground(self.appBackground,self.uiPage['image'])
         self.uiMake()
-        print self.uiObjects.keys()
+        print PLATFORM
